48dfbb0f03760538b98a127560fe87559b6e1a2a
Abdullah Yahya <abdullah747@gmail.com>
2016-10-12 14:51:12 -0700

    Added document root to all includes

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 	<head>
 		<title>Zabuun - Learn Egyptian Arabic for English speakers</title>
 		<meta name="description" content="">
-		<?php include '/layout/head.php';?>
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/layout/head.php';?>
 	</head>
 	<body>
-		<?php include '/layout/ie8.php';?>
-		<?php include '/layout/header.php';?>
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/layout/ie8.php';?>
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/layout/header.php';?>
 		<div class="content">
-			<?php include '/layout/side.php';?>
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/layout/side.php';?>
 			<div class="main">
 				<div class="location">
 					<p class="breadcrumbs">Dialogues > School Life > Buying Textbooks</p>
@@ -69,7 +69,7 @@
 				</div>
 			</div>
 		</div>
-		<?php include '/layout/footer.php';?>
-		<?php include '/layout/scripts.php';?>
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/layout/footer.php';?>
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/layout/scripts.php';?>
 	</body>
 </html>
