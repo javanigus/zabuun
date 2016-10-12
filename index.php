0767d42b87cf6b018ac5294d1d754a381f6916f6
Abdullah Yahya <abdullah747@gmail.com>
2016-10-12 14:49:31 -0700

    Added variable to include path

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ echo  $_SERVER['DOCUMENT_ROOT'];
 	</head>
 	<body>
 		<?php include '/layout/ie8.php';?>
-		<?php include '/home/javanigus/public_html/layout/header.php';?>
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/layout/header.php';?>
 		<div class="content">
 			<?php include '/layout/side.php';?>
 			<div class="main">
