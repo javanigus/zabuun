b9811242f446d73485d131cfdae3f5e8eea62050
Abdullah Yahya <abdullah747@gmail.com>
2016-10-12 17:04:04 -0700

    Updated include paths

--- assal-eswed.php
+++ assal-eswed.php
@@ -3,9 +3,13 @@
 	<head>
 		<title>Zabuun - Learn Egyptian Arabic for English speakers</title>
 		<meta name="description" content=">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/layout/head.php';?>
 	</head>
 	<body>
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/layout/ie8.php';?>
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/layout/header.php';?>
 		<div class="content">
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/layout/side.php';?>
 			<div class="main">
 				<div class="location">
 					<p class="breadcrumbs">Movies > Assal Eswed</p>
@@ -6215,5 +6219,7 @@
 				<!-- END SENTENCES -->
 			</div>
 		</div>
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/layout/footer.php';?>
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/layout/scripts.php';?>
 	</body>
 </html>
