2af199ac44f4962d6aa95c60a65c06bd9b6a8cee
Abdullah Yahya <abdullah747@gmail.com>
2016-10-19 16:58:56 -0700

    Added more resources

--- resources.php
+++ resources.php
@@ -17,6 +17,22 @@
                         <ul>
                               <li><a href="http://myarabickeyboard.org/yamli">Online Arabic Keyboard</a></li>
                         </ul>
+                        <h2>Egyptian Colloquial Arabic Textbooks</h2>
+                        <ul>
+                              <li><a href="https://www.amazon.com/dp/977416315X/ref=wl_it_dp_o_pC_S_ttl?_encoding=UTF8&colid=1S3OYIS3JQAKI&coliid=I1H3P40SEPF1G4">
+Kalaam Gamiil: An Intensive Course in Egyptian Colloquial Arabic: Volume 1</a></li>
+                              <li><a href="https://www.amazon.com/dp/9774164938/ref=wl_it_dp_o_pC_S_ttl?_encoding=UTF8&colid=1S3OYIS3JQAKI&coliid=I2JNA7STBW2PWP">
+Kalaam Gamiil: An Intensive Course in Egyptian Colloquial Arabic: Volume 2</a></li>
+                              <li><a href="https://www.amazon.com/dp/9774165640/ref=wl_it_dp_o_pC_S_ttl?_encoding=UTF8&colid=1S3OYIS3JQAKI&coliid=IP1Q81TJ4TYFE">Umm al-Dunya: Advanced Egyptian Colloquial Arabic</a></li>
+                              <li><a href="https://www.amazon.com/dp/0985816007/ref=wl_it_dp_o_pC_nS_ttl?_encoding=UTF8&colid=1S3OYIS3JQAKI&coliid=I1D973ONP6OMUY">
+Egyptian Colloquial Arabic Verbs: Conjugation Tables and Grammar</a></li>
+                              <li><a href="https://www.amazon.com/dp/0985816082/ref=wl_it_dp_o_pC_nS_ttl?_encoding=UTF8&colid=1S3OYIS3JQAKI&coliid=I2EFFY4UVY1DWC">Egyptian Colloquial Arabic Vocabulary</a></li>
+                        </ul>
+                        <h2>Modern Standard Arabic Books</h2>
+                        <ul>
+                              <li><a href="https://www.amazon.com/dp/0804843007/ref=wl_it_dp_o_pC_nS_ttl?_encoding=UTF8&colid=1S3OYIS3JQAKI&coliid=I1X5OXA78C8UKF">
+Arabic Stories for Language Learners: Traditional Middle Eastern Tales In Arabic and English (Audio CD Included)</a></li>
+                        </ul>
 			</div>
 		</div>
 		<?php include $_SERVER['DOCUMENT_ROOT'].'/layout/footer.php';?>
